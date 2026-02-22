976916a1e8c263bfba9a0b1f8c0a476fb6de2b77
Luka <khundadzeluka792@gmail.com>
2026-02-22 18:17:30 +0400

    ...

--- Application/UseCases/Schedules/Commands/AddTaskCommand.cs
+++ Application/UseCases/Schedules/Commands/AddTaskCommand.cs
@@ -35,7 +35,7 @@ public class AddTaskToChecklistCommandHandler(
             ?? throw new ApplicationArgumentException($"Checklist not found with Id={command.ChecklistId}");
 
         if (checklist.UserId != userId)
-            throw new ApplicationArgumentException($"user has no Checklist with ChecklistId={command.ChecklistId}");
+            throw new ApplicationArgumentException($"User has no Checklist with ChecklistId={command.ChecklistId}");
 
         var taskId = checklist.AddTask(
                 command.Name,
