418be0261642482a6c75976c06940f9b1ebb3098
Luka <khundadzeluka792@gmail.com>
2026-02-27 19:02:16 +0400

    RemoveTaskCommand

---
+++ Application/UseCases/Schedules/Commands/RemoveTaskCommand.cs
@@ -0,0 +1,45 @@
+using Application.Common.Exceptions;
+using Application.Interfaces.Data;
+using Application.Seedwork;
+using Application.UseCases.Identity;
+using Domain.Entities.Schedules.ValueObjects;
+
+namespace Application.UseCases.Schedules.Commands;
+
+public sealed record RemoveTaskCommand(
+        ChecklistId ChecklistId,
+        TaskId TaskId
+        ) : ICommand<EmptyReturn>;
+
+
+public class RemoveTaskCommandHandler(
+        IIdentityProvider identity,
+        IChecklistRepo checklistRepo,
+        IUserRepo userRepo,
+        IUnitOfWork uow)
+        : BaseCommandHandler<RemoveTaskCommand, EmptyReturn>(uow)
+{
+    protected override async Task<EmptyReturn> Execute(
+            RemoveTaskCommand command,
+            CancellationToken ct)
+    {
+        var userId = identity.GetCurrentUserId();
+        if (await userRepo.GetById(userId, ct) is null)
+            throw new ApplicationArgumentException($"User not found with Id={userId}");
+
+        var checklist = await checklistRepo.GetById(command.ChecklistId, ct)
+            ?? throw new ApplicationArgumentException($"Checklist not found with Id={command.ChecklistId}");
+
+        if (checklist.UserId != userId)
+            throw new ApplicationArgumentException($"User has no Checklist with ChecklistId={command.ChecklistId}");
+
+        checklist.RemoveTask(command.TaskId);
+
+        await checklistRepo.Update(checklist, ct);
+
+        return new EmptyReturn();
+    }
+}
+
+
+
