927509182ffcc4743b8e03434c2d9f32f16e0a03
Luka <khundadzeluka792@gmail.com>
2026-02-28 19:04:12 +0400

    ...

--- Application/UseCases/Schedules/Queries/GetTodaysChecklist.cs
+++
@@ -1,8 +0,0 @@
-using Application.Seedwork;
-using Domain.Entities.Schedules;
-
-namespace Application.UseCases.Schedules.Queries;
-
-
-public sealed record GetTodaysChecklist : IQuery<ChecklistState>;
-
