73d2a05887e8cd549672d7c702b583d4efed6345
Luka <khundadzeluka792@gmail.com>
2026-03-01 16:14:13 +0400

    adding DtoGenerator Library

---
+++ libraries/DtoGenerator/src/DtoGenerator/Class1.cs
@@ -0,0 +1,14 @@
+﻿namespace DtoGenerator;
+
+// <summary>
+//  Converts Domain Objects to Dtos
+// </summary>
+public interface IDtoGenerator
+{
+
+}
+
+public class DtoGenerator : IDtoGenerator
+{
+
+}
