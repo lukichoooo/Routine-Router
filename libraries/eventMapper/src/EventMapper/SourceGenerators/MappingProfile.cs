37bea5a6bbd5ea50b41a08178785076c19903654
Luka <khundadzeluka792@gmail.com>
2026-03-15 21:16:37 +0400

    ...

--- libraries/eventMapper/src/EventMapper/SourceGenerators/MappingProfile.cs
+++ libraries/eventMapper/src/EventMapper/SourceGenerators/MappingProfile.cs
@@ -4,11 +4,12 @@ namespace EventMapper.SourceGenerators;
 
 public static class MappingProfile
 {
+    // HARDCODED
     public static IImmutableSet<string> GetIgnoredOnPayloadPropNames
         => ImmutableHashSet.Create("AggregateId", "Version", "Timestamp");
-
     public static readonly string DbEventTypeName = "EventMapperAbstractions.DbEvents.IDbEvent";
     public static readonly string BaseEventTypeName = "EventMapperAbstractions.Events.IEvent`1";
+    // HARDCODED
 
     public static readonly string MapperNamespace = "Generated.EventMapper";
 
