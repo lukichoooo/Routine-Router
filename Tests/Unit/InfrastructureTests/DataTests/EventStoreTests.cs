b678cdb0c52c07f971c4c4327aec9ba50065b77f
Luka <khundadzeluka792@gmail.com>
2026-03-16 11:19:54 +0400

    event store tests load testing

--- Tests/Unit/InfrastructureTests/DataTests/EventStoreTests.cs
+++ Tests/Unit/InfrastructureTests/DataTests/EventStoreTests.cs
@@ -9,6 +9,7 @@ using Domain.Common.ValueObjects;
 using Microsoft.EntityFrameworkCore;
 using Domain.Entities.Users.Events;
 using Generated.EventMapper;
+using Application.Interfaces.Events;
 
 
 namespace InfrastructureTests.DataTests;
@@ -172,102 +173,90 @@ public class EventStoreTests
                 );
     }
 
-    //
-    // [Test]
-    // public async Task LoadAsyncTest_Success()
-    // {
-    //     // Arrange
-    //     var context = TestFactory.GetEventsContext();
-    //
-    //
-    //     var aggregateId = _fix.Create<UserId>();
-    //     var responseEvent = _fix.Build<UserCreated>()
-    //         .With(e => e.AggregateId, aggregateId)
-    //         .Create();
-    //
-    //
-    //     var onDbEvent = Event.From(
-    //             responseEvent,
-    //             _eventSerializer.ToPayload(responseEvent));
-    //     await context.AddAsync(onDbEvent);
-    //     await context.SaveChangesAsync();
-    //
-    //     IEventStore sut = new SQLiteEventStore(
-    //             _eventSerializer,
-    //             context);
-    //
-    //     // Act
-    //     var res = await sut.LoadAsync(
-    //             aggregateId: aggregateId,
-    //             ct: default);
-    //
-    //     // Assert
-    //     Assert.That(res, Is.EquivalentTo([responseEvent]));
-    // }
-    //
-    // [Test]
-    // public async Task LoadAsyncTest_Empty()
-    // {
-    //     // Arrange
-    //     var context = TestFactory.GetEventsContext();
-    //
-    //     var aggregateId = _fix.Create<UserId>();
-    //
-    //     IEventStore sut = new SQLiteEventStore(
-    //             _eventSerializer,
-    //             context);
-    //
-    //     // Act
-    //     var res = await sut.LoadAsync(
-    //             aggregateId: aggregateId,
-    //             ct: default);
-    //
-    //     // Assert
-    //     Assert.That(res, Has.Count.EqualTo(0));
-    // }
-    //
-    //
-    // [TestCase(0, null)]
-    // [TestCase(1, 1)]
-    // [TestCase(1, 3)]
-    // public async Task LoadAsyncTest_FromVersion(int fromVersion, int? toVersion)
-    // {
-    //     // Arrange
-    //     var context = TestFactory.GetEventsContext();
-    //
-    //     int versionCount = 1;
-    //
-    //     var aggregateId = _fix.Create<UserId>();
-    //     var responseEvents = _fix.Build<UserCreated>()
-    //         .With(e => e.AggregateId, aggregateId)
-    //         .With(e => e.Version, versionCount++)
-    //         .CreateMany()
-    //         .ToList();
-    //
-    //
-    //     var onDbEvents = responseEvents.Select(e =>
-    //             Event.From(e, _eventSerializer.ToPayload(e)));
-    //
-    //     await context.AddRangeAsync(onDbEvents);
-    //     await context.SaveChangesAsync();
-    //
-    //     IEventStore sut = new SQLiteEventStore(
-    //             _eventSerializer,
-    //             context);
-    //
-    //     // Act
-    //     var res = await sut.LoadAsync(
-    //             aggregateId: aggregateId,
-    //             ct: default,
-    //             fromVersion: fromVersion,
-    //             toVersion: toVersion);
-    //
-    //     // Assert
-    //     var expected = responseEvents
-    //         .Where(e => e.Version >= fromVersion
-    //                 && (toVersion == null || e.Version <= toVersion));
-    //
-    //     Assert.That(res, Is.EquivalentTo(expected));
-    // }
+
+    [Test]
+    public async Task LoadAsyncTest_Success()
+    {
+        // Arrange
+        var context = await TestFactory.GetEventContextAsync();
+
+        var aggregateId = _fix.Create<UserId>();
+        var responseEvent = _fix.Build<UserCreated>()
+            .With(e => e.AggregateId, aggregateId)
+            .Create();
+
+        var onDbEvent = Event.From(
+                responseEvent,
+                EventMapper.ToPayload(responseEvent));
+
+        await context.AddAsync(onDbEvent);
+        await context.SaveChangesAsync();
+
+        IEventStore sut = new SQLiteEventStore(context);
+
+        // Act
+        var res = await sut.Load(aggregateId: aggregateId, ct: default);
+
+        // Assert
+        Assert.That(res, Is.EquivalentTo([responseEvent]));
+    }
+
+    [Test]
+    public async Task LoadAsyncTest_Empty()
+    {
+        // Arrange
+        var context = await TestFactory.GetEventContextAsync();
+
+        var aggregateId = _fix.Create<UserId>();
+
+        IEventStore sut = new SQLiteEventStore(context);
+
+        // Act
+        var res = await sut.Load(aggregateId: aggregateId, ct: default);
+
+        // Assert
+        Assert.That(res, Has.Count.EqualTo(0));
+    }
+
+
+    [TestCase(0, null)]
+    [TestCase(1, 1)]
+    [TestCase(1, 3)]
+    public async Task LoadAsyncTest_FromVersion(int fromVersion, int? toVersion)
+    {
+        // Arrange
+        var context = await TestFactory.GetEventContextAsync();
+
+        int versionCount = 1;
+
+        var aggregateId = _fix.Create<UserId>();
+        var responseEvents = _fix.Build<UserCreated>()
+            .With(e => e.AggregateId, aggregateId)
+            .With(e => e.Version, versionCount++)
+            .CreateMany()
+            .ToList();
+
+
+        var onDbEvents = responseEvents.Select(e =>
+                Event.From(e, EventMapper.ToPayload(e)));
+
+        await context.AddRangeAsync(onDbEvents);
+        await context.SaveChangesAsync();
+
+        IEventStore sut = new SQLiteEventStore(context);
+        // Act
+        var res = await sut.Load(
+                aggregateId: aggregateId,
+                ct: default,
+                fromVersion: fromVersion,
+                toVersion: toVersion);
+
+        // Assert
+        var expected = responseEvents
+            .Where(e => e.Version >= fromVersion
+                    && (toVersion == null || e.Version <= toVersion));
+
+        Assert.That(res, Is.EquivalentTo(expected));
+    }
 }
 
